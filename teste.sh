4acacbf8f7cdfc976f6acb03c2d0007dd488a20e
Eliseu Junior <58912046+eliseu-modan@users.noreply.github.com>
2024-01-28 14:52:11 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -3,6 +3,19 @@
 # Certifique-se de estar no diretório do seu projeto
 cd ./frontend
 
+# Instale o nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | sh
+
+# Atualize o ambiente para incluir o nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Isso carrega o nvm
+
+# Instale a versão do Node.js desejada (no seu caso, 14)
+nvm install 14
+
+# Use a versão do Node.js recém-instalada
+nvm use 14
+
 # Instale as dependências (se necessário)
 npm install
 
