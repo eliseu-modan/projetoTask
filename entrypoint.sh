68d8dd4c16e9e15e61fe5f9943cfe9fd9a3bfc90
Eliseu Junior <58912046+eliseu-modan@users.noreply.github.com>
2024-02-02 12:06:42 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Iniciar o serviço frontend
-npm start
+npm test
 
 # Parar o serviço após um tempo adequado para execução dos testes
 sleep 10
 
+npm start
 # Executar os testes
-npm test
