c8204eb4bf0f552f167ebf5cbeb419c1cd4eeeb5
Eliseu Junior <58912046+eliseu-modan@users.noreply.github.com>
2024-02-09 10:01:51 -0300

    Update constants.jsx

--- frontend/src/contexts/service/constants.jsx
+++ frontend/src/contexts/service/constants.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 export const HOST_URL =
   process.env.NODE_ENV === "development"
-    ? "http://172.22.0.2:3333"
+    ? "http://backend:3333"
     : `${window.location.origin}`;
 
 export const BASE_URL = (axios.defaults.baseURL =
-  "http://172.22.0.2:3333/api");
+  "http://backend:3333/api");
 
 export const serviceDefaultConfig = {
   baseURL: BASE_URL,
